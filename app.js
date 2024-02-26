ced4b39bd3d88041bf7be7298e4d710db5a45162
Jao16905 <Jao16905>
2024-02-26 15:20:34 -0500

    Ajeita o código

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app)
 const socketio = require("socket.io")(server);
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const path = require("path");
+const userRouter = require("./routes/routes")
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/public/templates"));
+
+app.use("/", userRouter)
+
+app.listen(process.env.PORT, () =>{
+    console.log("Server running")
+})
+
